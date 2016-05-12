bc748a15744ff37b9956851d47ac04fc4d068eef
Eedrah <eedrah@users.noreply.github.com>
2016-05-13 09:50:45 +1200

    WIP

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-alert('you are on duolingo practice!')
-console.log('index loaded')
+console.log('loaded!')
+
+function promptUser (alarm) {
+  console.log('in function')
+  if (alarm.name !== 'DuolingoPrompter') return
+  alert('You need to practice!')
+}
+chrome.alarms.onAlarm.addListener(promptUser)
+
+chrome.alarms.create('DuolingoPrompter', {
+  periodInMinutes: 0.1
+})
+console.log('finished')
